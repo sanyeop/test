9cd9ecb8ec496ee5d340b0faa16ca0245580a08a
sanyeop <cubetree10@gmail.com>
2026-05-19 12:06:39 +0900

    Add files via upload

---
+++ rocket-sim/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    backgroundColor: '#0b0e14',
+    title: 'Rocket Launch Simulator',
+  });
+
+  // Remove default menu
+  win.setMenu(null);
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    // win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
